a89cfe23411f0fa3585772710cde1362dfff6fea
Hayes <HayesV@sharpusa.com>
2025-04-04 04:08:31 -0700

    Fix incorrect alias imports

--- src/SolidHedgeSite.jsx
+++ src/SolidHedgeSite.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Card, CardContent } from "../components/ui/card";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
 import { Mail } from "lucide-react";
 
 export default function SolidHedgeSite() {
